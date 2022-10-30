4ea47dee4c806e354c7fe64f194498c882b0f0af
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-10-30 15:57:00 +0900

    End of course
    Problem : I can't use my own font.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,34 @@ const colorOption = Array.from(document.querySelectorAll(".colorOption"));
 const fillMode = document.querySelector(".fillMode");
 const strokeMode = document.querySelector(".strokeMode");
 const fileInput = document.querySelector("#file");
+const textInput = document.querySelector("#textInput");
+const saveBtn = document.querySelector("#saveBtn");
+const fontSizeInput = document.querySelector(".font-size");
+const clearBtn = document.querySelector(".clear");
 
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
+let fontsize = fontSizeInput.value;
+// const fonturl = "src/ulsanjunggu.ttf";
+// let font = new FontFace(
+//   "fonttest",
+//   "url(https://fonts.gstatic.com/s/notosanskr/v13/PbykFmXiEBPT4ITbgNA5Cgm203Tq4JJWq209pU0DPdWuqxJFA4GNDCBYtw.0.woff2)"
+// );
+// font.load().then(
+//   () => {
+//     ctx.font = `${fontsize}px fonttest`;
+//     console.log(ctx.font);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
 
 function onMove(event) {
   if (isPainting) {
@@ -77,6 +97,41 @@ function onFileChange(event) {
   fileInput.value = null;
 }
 
+function onDoubleClick(event) {
+  const text = textInput.value;
+  if (text !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    updateFont();
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myimage.png";
+  a.click();
+}
+
+function onFontSizeChange() {
+  fontsize = fontSizeInput.value;
+  updateFont();
+}
+
+function updateFont() {
+  ctx.font = `${fontsize}px sans-serif`;
+}
+
+function onClearClick() {
+  ctx.save();
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.restore();
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -90,3 +145,7 @@ fillMode.addEventListener("click", onClickFillMode);
 strokeMode.addEventListener("click", onClickStrokeMode);
 canvas.addEventListener("click", onClickCanvas);
 fileInput.addEventListener("change", onFileChange);
+canvas.addEventListener("dblclick", onDoubleClick);
+saveBtn.addEventListener("click", onSaveClick);
+fontSizeInput.addEventListener("change", onFontSizeChange);
+clearBtn.addEventListener("click", onClearClick);
